803b181e04784a74d74071b7656900c00520ac74
Joel-100 <joelkugonza@gmail.com>
2023-03-27 20:38:58 +0300

    portfolio-update

---
+++ js/project-list.js
@@ -0,0 +1,158 @@
+const projectList = [
+  {
+    id: 1,
+    title: 'Africa Tech Week 2022',
+    featuredImage: {
+      url: '../images/africa-tech-week-img.PNG',
+      alt: 'capstone-project-1',
+    },
+    description: {
+      less: 'The goal of the Microverse Capstone Project for Module I: (The Portal) is to create a website with an about page and a home page for an online conference event. Built with JavaScript, CSS, and HTML.',
+      more: 'The goal of the Microverse Capstone Project for Module I: (The Portal) is to create a website with an about page and a home page for an online conference event. Built with JavaScript, CSS, and HTML.',
+    },
+    technologies: ['HTML', 'CSS', 'JavaScript (ES6)'],
+    projectDetail: {
+      client: 'microverse',
+      role: 'front end dev',
+      year: 2022,
+    },
+    link: {
+      source: 'https://github.com/Joel-100/capstone-1',
+      live: 'https://michael-duke.github.io/Capstone-I/',
+    },
+  },
+  {
+    id: 2,
+    title: 'Fast Food API',
+    featuredImage: {
+      url: 'images/fast-food-img.jpg',
+      alt: 'fast-food-img',
+    },
+    description: {
+      less: 'This is based on a web app that takes up the data from the API and displays it along with counters for a number of likes and reviews..',
+      more: 'This is based on a web app that takes up the data from the API and displays it along with counters for a number of likes and reviews..',
+    },
+    technologies: ['HTML', 'Tailwind CSS', 'JavaScript (ES6)'],
+    projectDetail: {
+      client: 'microverse',
+      role: 'front end dev',
+      year: 2022,
+    },
+    link: {
+      source: 'https://github.com/Joel-100',
+      live: 'https://github.com/Joel-100/my-portfolio/tree/deploy-web',
+    },
+  },
+  {
+    id: 3,
+    title: 'Math Magicians',
+    featuredImage: {
+      url: 'images/math-image.jpg',
+      alt: 'math-image',
+    },
+    description: {
+      less: 'A website for math geeks where i created 3 pages including home, calculator, and quote. I also added my own styles to improve the look and feel of the website.',
+      more: 'A website for math geeks where i created 3 pages including home, calculator, and quote. I also added my own styles to improve the look and feel of the website.',
+    },
+    technologies: ['HTML', 'Tailwind CSS', 'JavaScript (ES6)', 'Webpack'],
+    projectDetail: {
+      client: 'microverse',
+      role: 'back end dev',
+      year: 2022,
+    },
+    link: {
+      source: 'https://github.com/Joel-100',
+      live: 'https://github.com/Joel-100/my-portfolio/tree/deploy-web',
+    },
+  },
+  {
+    id: 4,
+    title: 'Space Travelers Hub',
+    featuredImage: {
+      url: 'images/space-image.jpg',
+      alt: 'space-image',
+    },
+    description: {
+      less: 'A website that consumes the SpaceX API and allows you to reserve rockets and join missions.',
+      more: 'A website that consumes the SpaceX API and allows you to reserve rockets and join missions.',
+    },
+    technologies: ['HTML', 'Taiwind CSS', 'ES6', 'Webpack'],
+    projectDetail: {
+      client: 'microverse',
+      role: 'front end dev',
+      year: 2022,
+    },
+    link: {
+      source: 'https://github.com/Joel-100',
+      live: 'https://github.com/Joel-100/my-portfolio/tree/deploy-web',
+    },
+  },
+  {
+    id: 5,
+    title: 'African Countries',
+    featuredImage: {
+      url: 'images/african-countries-image.jpg',
+      alt: 'react cap african countries',
+    },
+    description: {
+      less: 'African Countries is a web app that displays all countries using an API and gives details on every country..',
+      more: 'African Countries is a web app that displays all countries using an API and gives details on every country..',
+    },
+    technologies: ['HTML', 'Tailwind CSS', 'React', 'Redux', 'React Router', 'Redux-thunk'],
+    projectDetail: {
+      client: 'microverse',
+      role: 'fullstack dev',
+      year: 2022,
+    },
+    link: {
+      source: 'https://github.com/Joel-100/react-capstone',
+      live: 'https://react-capstone-one.vercel.app/',
+    },
+  },
+  {
+    id: 6,
+    title: 'Budget APP',
+    featuredImage: {
+      url: 'images/budget-img.PNG',
+      alt: 'budget image',
+    },
+    description: {
+      less: 'This project is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
+      more: 'This project is about building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
+    },
+    technologies: ['CSS', 'Ruby', 'Ruby on Rails', 'PostgreSQL'],
+    projectDetail: {
+      client: 'microverse',
+      role: 'fullstack dev',
+      year: 2023,
+    },
+    link: {
+      source: 'https://github.com/Joel-100/ror-budget-app',
+      live: 'https://ror-budget-app.onrender.com/',
+    },
+  },
+  {
+    id: 7,
+    title: 'Cherry Rooms',
+    featuredImage: {
+      url: 'images/Cherry-rooms.PNG',
+      alt: 'cherry rooms image',
+    },
+    description: {
+      less: 'Cherry Rooms is a full-stack ROR/React project designed for the purpose of booking rooms in hotels, motels, or any other accommodation service. This project provides an intuitive and user-friendly interface that allows users to easily browse and reserve rooms based on their preferences and availability. The project is built using React, HTML, CSS,.',
+      more: 'Cherry Rooms is a full-stack ROR/React project designed for the purpose of booking rooms in hotels, motels, or any other accommodation service. This project provides an intuitive and user-friendly interface that allows users to easily browse and reserve rooms based on their preferences and availability. The project is built using React, HTML, CSS,.',
+    },
+    technologies: ['CSS', 'Ruby', 'Ruby on Rails', 'PostgreSQL', 'React', 'Redux', 'React Router', 'Redux-toolkit', 'REST API'],
+    projectDetail: {
+      client: 'microverse',
+      role: 'fullstack dev',
+      year: 2023,
+    },
+    link: {
+      source: 'https://github.com/Joel-100/Cherry-Rooms-front-end',
+      live: 'https://cherryrooms.netlify.app/',
+    },
+  },
+];
+
+export default projectList;
